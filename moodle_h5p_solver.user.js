ddf261afa8324c732ac378e0b3d2807ca4a6957a
rkay <60544466+khanxbahria@users.noreply.github.com>
2023-05-29 13:46:28 +0300

    refactor

--- moodle_h5p_solver.user.js
+++ moodle_h5p_solver.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Moodle H5P Solver
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Moodle H5P Solver solves the H5P interactions and obtains the full score without knowing the correct answers.
 // @author       khanxbahria
 // @match        https://*/grade/report/user/index.php?*
@@ -9,82 +9,89 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-	'use strict';
+"use strict";
 
-	var table = document.querySelector("#region-main > div > table")
-	var grade_col;
-	for (var j = 0, col; col = table.rows[0].cells[j]; j++) {
-		if (col.id == 'grade') {
-			grade_col = j;
-			break
-		}
-	}
+const extractVideoID = tr => {
+    const aTag = tr.querySelector('th.item > a, th.column-itemname > a');
+    const tdTag = tr.querySelector('td.column-grade');
+    if (!aTag || !aTag.href.includes('hvp') || tdTag.innerText !== "-")
+        return;
+    const url = new URL(aTag.href);
+    return url.searchParams.get('id')
+};
 
-	var vids = [];
-	for (var i = 2, row; row = table.rows[i]; i++) {
-		if (row.cells.length <= grade_col) continue;
-		if (row.cells[grade_col].getInnerHTML() != "-") continue;
-		if (row.cells[0].childNodes.length == 0 || row.cells[0].childNodes[0].href == undefined) continue;
+const processTable = () => {
+    const table = document.querySelector('#region-main > div > table');
+    const tbody = table.tBodies[0];
+    const rows = tbody.querySelectorAll('tr');
+    const videoIDs = Array.from(rows)
+        .map(extractVideoID)
+        .filter(Boolean);
+    return videoIDs;
 
-		var url_string = row.cells[0].childNodes[0].href
-		//console.log(url_string);
-		var url = new URL(url_string);
-		if (url.pathname != "/mod/hvp/grade.php") continue;
-		var vid = url.searchParams.get('id');
-		vids.push(vid)
+};
 
-	}
+const getStringBetween = (str, start, end) => {
+    const betweenExp = new RegExp(`${start}(.*?)${end}`);
+    return str.match(betweenExp)[1];
+};
+
+const fetchVideoData = async videoID => {
+    const path = `/mod/hvp/view.php?id=${videoID}`
+    const resp = await fetch(path);
+    const content = await resp.text();
+    const cid = getStringBetween(content, 'cid-', '"');
+    const token = getStringBetween(content, 'token=', '&');
+    return {
+        contextId: videoID,
+        contentId: cid,
+        token: token
+    }
+};
+
+const updateScore = async (videoData) => {
+    const score = 1;
+    const maxScore = 1;
+    const finished = Math.floor(Date.now() / 1000);
+    //opened 10 minutes earlier than now
+    const opened = finished - 10 * 60;
+
+    const body = `contentId=${videoData.contentId}
+&score=${score}&maxScore=${maxScore}
+&opened=${opened}&finished=${finished}`;
+
+    const path = `/mod/hvp/ajax.php?contextId=
+${videoData.contextId}&token=${videoData.token}
+&action=set_finished`;
+
+    const resp = await fetch(path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body
+    });
+    return await resp.text();
+}
+
+const solveOne = async (videoID) => {
+    const videoData = await fetchVideoData(videoID);
+    return await updateScore(videoData);
+}
+
+const main = async () => {
+    const videoIDs = processTable();
+    console.log(videoIDs);
+    const prompt = `${videoIDs.length} unwatched videos.\nClick OK to resolve.`
+    if (videoIDs.length > 0 && confirm(prompt)) {
+        const promises = videoIDs.map(solveOne);
+        const results = await Promise.allSettled(promises);
+        console.log(results);
+        alert("All done!");
+        window.location.reload();
+    }
+}
 
 
-	console.log(vids)
-	if (vids.length > 0 && confirm(vids.length + " unwatched videos.\n Click OK to resolve.")) {
-		var start_time = Math.floor(Date.now() / 1000) - 24 * 60 * 60;
 
-		function getStringBetween(str, start, end) {
-			const result = str.match(new RegExp(start + "(.*?)" + end));
-
-			return result[1];
-		}
-		for (let i = 0, vid; vid = vids[i]; i++) {
-			let xhr = new XMLHttpRequest();
-			xhr.open("GET", "/mod/hvp/view.php?id=" + vid, true);
-			xhr.onload = function(e) {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200) {
-						let data = xhr.responseText
-						let cid = getStringBetween(data, "cid-", '"')
-
-						i = data.indexOf("token=")
-						let token = getStringBetween(data, "token=", "&")
-
-						let finalxhr = new XMLHttpRequest();
-
-
-						finalxhr.open("POST", "/mod/hvp/ajax.php?contextId=" + vid + "&token=" + token + "&action=set_finished", true);
-						finalxhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-						finalxhr.onreadystatechange = function() {
-							if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-								if (vid == vids[vids.length - 1]) {
-									alert("All done!")
-									window.location.reload();
-
-								}
-							}
-						}
-						finalxhr.send("contentId=" + cid + "&score=1&maxScore=1&opened=" + String(start_time) + "&finished=" + String(start_time + 10 * 60));
-					} else {
-						console.error(xhr.statusText);
-					}
-				}
-			};
-			xhr.send(null)
-
-
-			start_time += 10 * 60
-		}
-
-
-
-	}
-})();
+main();
